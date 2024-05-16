559dca443aaf53fcc18c73e25cb91f8b210ee7f9
Adam Momen <adammuman81@gmail.com>
2024-05-16 22:13:26 +0800

    add: hardcoded sentry dns url

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ setupEnv();
 const isDev = process.env.NODE_ENV == "development"
 
 Sentry.init({
-  dsn: process.env.SENTRY_DNS,
+  dsn: "https://9c398efc0837f0b1b93cc4d2f92bb710@o320781.ingest.us.sentry.io/4507266493775872",
   environment: process.env.NODE_ENV,
   tracesSampleRate: isDev? 1.0 : 0.1,
   sendDefaultPii: true,
