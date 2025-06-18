5ed920286d38e5fca63a4140dff54d5ec36ada28
HardlyDifficult <hardlydiff@gmail.com>
2025-06-18 07:48:21 -0400

    Restore submodule

--- src/chain-operations/canton/clientConfig.js
+++ src/chain-operations/canton/clientConfig.js
@@ -1,5 +1,5 @@
-import { TransferAgentConfig } from "./lib/config";
-import { FairmintClient } from "./lib/fairmintClient";
+import { TransferAgentConfig } from "./lib/fairmint-canton/scripts/src/helpers/config";
+import { FairmintClient } from "./lib/fairmint-canton/scripts/src/helpers/fairmintClient";
 
 const config = new TransferAgentConfig(true);
 const client = new FairmintClient(config);
